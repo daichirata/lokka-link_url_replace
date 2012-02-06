0fd1dfd4788d5f49783b2e1674a87c9ff0ae8d5f
Dach-h <bunny.hop.md@gmail.com>
2012-02-06 11:53:50 +0900

    refactor

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lokka Link Url Replace
+
+## Installation
+
+    $ cd APP_ROOT/public/plugins/
+    $ git clone git@github.com:daic-h/lokka-link_url_replace.git
+
+## How to Use
+
+e.g.
+
+    [url:http://lokka.org/]
+
+It is replaced.
+
+    "<a href="http://lokka.org/">Lokka - CMS for Cloud</a>"
+
+
+
--- lib/lokka/link_url_replace.rb
+++ lib/lokka/link_url_replace.rb
@@ -1,11 +1,10 @@
 module Lokka
-  module LinkUrl
+  module LinkUrlReplace
     def self.registered(app)
       %w(posts posts/* pages pages/*).each do |suburl|
         app.before("/admin/#{suburl}") do
-          if @request.env['REQUEST_METHOD'] =~ /POST|PUT/ && 
-             (body = (params[:post] && params[:post][:body]))
-            body.force_encoding("utf-8").gsub!(/\[url:(.*?)\]/u){ LinkUrl::Util.link($1) }
+          if @request.env['REQUEST_METHOD'] =~ /POST|PUT/ && (body = (params[:post] && params[:post][:body]))
+            body.force_encoding("utf-8").gsub!(/\[url:(.*?)\]/u){ LinkUrlReplace::Util.replace($1) }
           end
         end
       end
@@ -14,8 +13,7 @@ module Lokka
     module Util
       require 'open-uri'
 
-      def self.link(url)
-#        title = URI.parse(url)).body.scan(/<title>(.*)<\/title>/i).join("").force_encoding("utf-8")
+      def self.replace(url)
         title = open(url, "r"){|o| o.read.scan(/<title>(.*)<\/title>/i).join("")} rescue url
         "<a href=\"#{url}\" target=\"_blank\">#{title}</a>"
       end
