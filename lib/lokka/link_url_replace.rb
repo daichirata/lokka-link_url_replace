a2a6b94cb436e4e58b62642cf4fc4ce8cbc62a30
Daichi Hirata <bunny.hop.md@gmail.com>
2012-02-24 19:19:34 +0900

    大幅に変更中

--- lib/lokka/link_url_replace.rb
+++ lib/lokka/link_url_replace.rb
@@ -1,22 +1,71 @@
 module Lokka
   module LinkUrlReplace
     def self.registered(app)
-      %w(posts posts/* pages pages/*).each do |suburl|
-        app.before("/admin/#{suburl}") do
-          if @request.env['REQUEST_METHOD'] =~ /POST|PUT/ && (body = (params[:post] && params[:post][:body]))
-            body.force_encoding("utf-8").gsub!(/\[url:(.*?)\]/u){ LinkUrlReplace::Util.replace($1) }
+      %w(posts posts/* pages pages/*).each do |sub_url|
+        app.before("/admin/#{sub_url}") do
+          if request.request_method =~ /POST|PUT/ && (body = (params[:post] && params[:post][:body]))
+            Replace.replace!(body)
           end
         end
       end
     end
 
-    module Util
+    module Replace
       require 'open-uri'
 
-      def self.replace(url)
-        title = open(url, "r"){|o| o.read.scan(/<title>(.*)<\/title>/i).join("")} rescue url
+      @@patterns = []
+
+      def self.replace!(body)
+        @@patterns.each do |pattern|
+          pattern.call(body)
+        end
+      end
+
+      def self.replace_pattarn(suffix, &block)
+        regexp = Regexp.compile("[#{suffix}:(.*?)\]", nil, "u")
+
+        @@patterns << lambda do |body|
+          body.force_encoding("utf-8").gsub!(regexp) { block.call($1) }
+        end
+      end
+
+      # Example:
+      #   [url:http://lokka.org]
+      #
+      #   <a href="http://lokka.org">Lokka - CMS for Cloud</a>
+      #
+      replace_pattarn :url do |url|
+        title = open(url, "r") { |o| o.read.scan(/<title>(.*)<\/title>/i).join("") } rescue url
         "<a href=\"#{url}\" target=\"_blank\">#{title}</a>"
       end
+
+      # Example:
+      #   [github:komagata/lokka]
+      #
+      #   <a href="https://github.com/komagata/lokka" target="_blank">komagata/lokka</a>
+      #
+      replace_pattarn :github do |url|
+        name, repo = url.split("/")
+        "<a href=\"https://github.com/#{name}/#{repo}\" target=\"_blank\">#{url}</a>"
+      end
+
+      #
+      # [TODO]
+      #
+      # Example:
+      #   [twitter:Daic_h]
+      #
+      #   <a href="https://github.com/komagata/lokka" target="_blank">komagata/lokka</a>
+      #
+
+      #
+      # [TODO]
+      #
+      # Example:
+      #   [gist:1804145]
+      #
+      #   <script src="https://gist.github.com/1804145.js?file=import_twilist.rb"></script>
+      #
     end
   end
 end
